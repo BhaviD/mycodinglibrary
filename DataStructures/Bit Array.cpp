4db2fedc142c88c26b145fc20993ec6386eaae40
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2016-07-17 01:36:42 +0530

    Again correcting indentation

--- DataStructures/Bit Array.cpp
+++ DataStructures/Bit Array.cpp
@@ -8,81 +8,81 @@ struct Bit
 {
     unsigned int val : 1;
 };
- 
+
 class BitArray
 {
     private:
-		Bit bits[BIT_ARRAY_LENGTH];
-		int size{ 0 };
-		int value{ 0 };
-		void RecalculateSize()
-		{
-			for (int pos = 1; pos < BIT_ARRAY_LENGTH; ++pos)
-			{
-				if (bits[pos].val)
-					size = pos;
-			}
-		}
+        Bit bits[BIT_ARRAY_LENGTH];
+        int size{ 0 };
+        int value{ 0 };
+        void RecalculateSize()
+        {
+            for (int pos = 1; pos < BIT_ARRAY_LENGTH; ++pos)
+            {
+                if (bits[pos].val)
+                size = pos;
+            }
+        }
 
     public:
         BitArray(int num = 0)
         {
-			value = num;
-			for (int pos = 1; pos < BIT_ARRAY_LENGTH; ++pos)
-			{
-				bits[pos].val = (value & (1 << (pos - 1))) >> (pos - 1);
-				if (bits[pos].val)
-					size = pos;
-			}
+            value = num;
+            for (int pos = 1; pos < BIT_ARRAY_LENGTH; ++pos)
+            {
+                bits[pos].val = (value & (1 << (pos - 1))) >> (pos - 1);
+                if (bits[pos].val)
+                    size = pos;
+            }
         }
 
-		int Size()
-		{
-			return size;
-		}
+        int Size()
+        {
+            return size;
+        }
 
-		unsigned int GetBit(int pos)
-		{
-			return bits[pos].val;
-		}
+        unsigned int GetBit(int pos)
+        {
+            return bits[pos].val;
+        }
 
-		void SetBit(int pos)
-		{
-			if (bits[pos].val)
-				return;
+        void SetBit(int pos)
+        {
+            if (bits[pos].val)
+                return;
 
-			bits[pos].val = 1;
-			if (pos > size)
-				size = pos;
+            bits[pos].val = 1;
+            if (pos > size)
+                size = pos;
 
-			value |= 1 << (pos - 1);
-		}
+            value |= 1 << (pos - 1);
+        }
 
-		void UnsetBit(int pos)
-		{
-			if (!bits[pos].val)
-				return;
+        void UnsetBit(int pos)
+        {
+            if (!bits[pos].val)
+                return;
 
-			bits[pos].val = 0;
-			if (pos == size)
-				RecalculateSize();
+            bits[pos].val = 0;
+            if (pos == size)
+                RecalculateSize();
 
-			value ^= 1 << (pos - 1);
-		}
+            value ^= 1 << (pos - 1);
+        }
 
         int Value()
         {
             return value; 
         }
 
-		void ShowBinaryForm()
-		{
-			cout << "(" << value << "): ";
-			for (int pos = size; pos > 0; --pos)
-			{
-				cout << GetBit(pos) << " ";
-			}
-		}
+        void ShowBinaryForm()
+        {
+            cout << "(" << value << "): ";
+            for (int pos = size; pos > 0; --pos)
+            {
+                cout << GetBit(pos) << " ";
+            }
+        }
 };
 
 int main()
@@ -91,18 +91,18 @@ int main()
     cout << "Enter 32 bit integer value(0 - (2^32 - 1)): ";
     cin >> num;
 
-	BitArray* A = new BitArray {num};
+    BitArray* A = new BitArray {num};
 
     cout << "\nThe value is: " << A->Value() << endl;
-	A->ShowBinaryForm();
+    A->ShowBinaryForm();
 
-	A->SetBit(4);
-	cout << "\nThe value is: " << A->Value() << endl;
-	A->ShowBinaryForm();
+    A->SetBit(4);
+    cout << "\nThe value is: " << A->Value() << endl;
+    A->ShowBinaryForm();
 
-	A->UnsetBit(7);
-	cout << "\nThe value is: " << A->Value() << endl;
-	A->ShowBinaryForm();
+    A->UnsetBit(7);
+    cout << "\nThe value is: " << A->Value() << endl;
+    A->ShowBinaryForm();
 
     return 0;
 }
