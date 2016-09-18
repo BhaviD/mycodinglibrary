802824cb42924c22e9a857e8f319a815862276d1
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2016-09-18 17:03:50 +0530

    Using colon ':' as the delimiter in getline function

--- STL/String/getline.cpp
+++ STL/String/getline.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 int main()
 {
-    string str;
+    string str, userName, userPwd;
     ifstream fin("getline_input.txt");
     fin >> str;
     cout << "value of str using >> operator: " << str << endl;
     fin.seekg(0, ios::beg);
     getline(fin, str);
     cout << "value of str using getline: " << str << endl;
+
+    getline(fin, userName, ':');
+    getline(fin, userPwd);
+
+    cout << "UserName = " << userName << endl
+         << "UserPwd = " << userPwd << endl;
     return 0;
 }
