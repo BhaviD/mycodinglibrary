e396e9a9d03c2e3232bdb7e661f1f552e4534dfb
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2016-09-27 23:35:00 +0530

    Adding comments and extra logs for better understanding, resetting iosflags

--- Manipulators/setw_+_setprecision.cpp
+++ Manipulators/setw_+_setprecision.cpp
@@ -5,26 +5,30 @@ using namespace std;
 int main () 
 {
     cout << setw(10);
-    cout << 77 << endl;
-    cout << 99 << endl;
+    cout << 77 << "          -->  width = 10\n";
+    cout << 99 << "          -->  setw is not \"Sticky\"\n";
     
     double f = 3.141592653589;
-    streamsize ss = cout.precision();
+    streamsize ss = cout.precision();       // save original precision value
+    ios_base::fmtflags flags = cout.flags();    // save original iosflags
     cout << "Initial precision = " << ss << endl;
     cout << "Value = " << f << endl;
-    cout << setw(10) << setprecision(4) << f << endl;
+    cout << setprecision(4) << f << "    -->  setprecision(4) rounds off the value\n";
 
-    cout.precision (10);
+    cout.precision (13);
     cout << "Longer value = " << f << endl;
+    cout << "Longer value = " << f << "       --> setprecision is \"Sticky\"\n";
 
     cout.precision (ss);
-    cout << "Original value = " << f << endl;
+    cout << "Original precision restored = " << f << endl;
 
-    cout << "Longer and original value = "
+    cout << "Longer and original precision values = "
         << setprecision(10) << f << ' '
         << setprecision(ss) << f << endl;
 
     cout << fixed;
-    cout << "Original value = " << f << endl;
+    cout << "Using std::fixed with original precision = " << setprecision(ss) << f << endl;
+    cout.flags(flags);
+    cout << "Restoring original iosflags = " << f << endl;
     return 0;
 }
