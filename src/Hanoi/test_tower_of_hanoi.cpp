70ec5eb64c2d043d55793a0e13c5c49de96f7ede
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2016-10-05 00:06:59 +0530

    Removing the forward declaration of Hanoi function

--- src/Hanoi/test_tower_of_hanoi.cpp
+++ src/Hanoi/test_tower_of_hanoi.cpp
@@ -10,24 +10,19 @@
 
 using namespace std;
 
-// move n disks from initNeedle to endNeedle, using tempNeedle
-// for intermediate storage of the disks. 
-void hanoi(int n, const string& initNeedle,
-              const string& endNeedle, const string& tempNeedle);
-
 int main()
 {
-   // number of disks and the needle names
-   int n;
-   string beginneedle  = "A",
+    // number of disks and the needle names
+    int n;
+    string beginneedle  = "A",
           middleneedle = "B",
           endneedle    = "C";
 
-   // prompt for n and solve the puzzle for n disks
-   cout << "Enter the number of disks: ";
-   cin >> n;
-   cout << "The solution for n = " << n << endl;
-   hanoi(n, beginneedle, endneedle, middleneedle);
+    // prompt for n and solve the puzzle for n disks
+    cout << "Enter the number of disks: ";
+    cin >> n;
+    cout << "The solution for n = " << n << endl;
+    hanoi(n, beginneedle, endneedle, middleneedle);
 
     return 0;
 }
