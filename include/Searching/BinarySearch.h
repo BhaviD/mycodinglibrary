bb263ac7d7888c2e87bae9ed1a174d320b9b7690
BhaviD <bhavi.dhingra@gmail.com>
2016-10-15 16:19:25 +0530

    Removing int version of Binary Search

--- include/Searching/BinarySearch.h
+++ include/Searching/BinarySearch.h
@@ -3,11 +3,6 @@
 
 using namespace std;
 
-// perform a binary search of an integer array for a target
-// in the index range [first, last). return the index of a
-// match or last if the target is not in arr
-int IterativeBinarySearch(const int arr[], int first, int last, int target);
-
 // perform a binary search of an array for a target
 // in the index range [first, last). return the index of a
 // match or last if the target is not in arr
@@ -16,28 +11,6 @@ int IterativeBinarySearch(const T arr[], int first, int last, const T& target);
 
 template <typename T>
 int RecursiveBinarySearch(const T arr[], int first, int last, const T& target);
-
-int IterativeBinarySearch(const int arr[], int first, int last, int target)
-{
-    int mid;                    // index of the midpoint
-    int midValue;               // object that is assigned arr[mid]
-    int origLast = last;        // save original value of last
-
-    while (first < last)        // test for nonempty sublist
-    {
-        mid = (first + last) / 2;
-        midValue = arr[mid];
-        if (target == midValue)
-            return mid;         // have a match
-        // determine which sublist to search
-        else if (target < midValue)
-            last = mid;         // search lower sublist. reset last
-        else
-            first = mid + 1;    // search upper sublist. reset first
-    }
-
-    return origLast;            // target not found
-}
 
 template <typename T>
 int IterativeBinarySearch(const T arr[], int first, int last, const T& target)
