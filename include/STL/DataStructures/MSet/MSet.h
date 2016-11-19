59ff35e5ddfbd3c5fd6705c0afe1027d1fde1f46
Bhavi Dhingra <bhavi.dhingra@gmail.com>
2016-11-19 14:27:41 +0530

    Adding implementation of Multi Set class - using MBSTree class

--- include/STL/DataStructures/MSet/MSet.h
+++ include/STL/DataStructures/MSet/MSet.h
@@ -1,186 +1,207 @@
-#ifndef _SET_H_
-#define _SET_H_
+#ifndef _MSET_H_
+#define _MSET_H_
 
 #include <utility>
+#include "MBSTree.h"    // MBSTree class
 
-#include "BSTree.h"    // BSTree class
-
-// implements a set which does not contain duplicate data values
+// implements a set which contains duplicate data values
 template <typename T>
-class Set
+class MSet
 {
     public:
-
-        // Set iterators are simply BSTree iterators
-        typedef typename BSTree<T>::iterator iterator;
-        typedef typename BSTree<T>::const_iterator const_iterator;
+        // MSet iterators are simply MBSTree iterators
+        typedef typename MBSTree<T>::iterator iterator;
+        typedef typename MBSTree<T>::const_iterator const_iterator;
 
         // default constructor
-        Set();
+        MSet();
 
-        // build a set whose data are determined by pointer range
+        // build a multiset whose data are determined by pointer range
         // [first, last)
-        Set(T *first, T *last);
+        MSet(T *first, T *last);
 
-        // is the set empty?
+        // is the multiset empty?
         bool empty() const;
 
-        // return the number of elements in the set
+        // return the number of elements in the multiset
         int size() const;
 
-        // search for item in the set and return an iterator
-        // pointing at it, or end() if it is not found
+        // return the number of duplicate occurrences of item
+        // in the multiset
+        int count(const T& item) const;
+
+        // search for item in the multiset and return an iterator
+        // pointing at the first occurrence matching item or end() 
+        // if it is not found
         iterator find (const T& item);
 
         // constant version
         const_iterator find (const T& item) const;
 
-        // if item is not in the set, insert it and return a pair
-        // whose first element is an iterator pointing to the
-        // new element and whose second element is true.
-        // otherwise, return a pair whose first element is an
-        // iterator pointing at the existing element and whose
-        // second element is false
-        // Postcondition: the set size increases by 1 if item is
-        // not in the set
-        std::pair<iterator,bool> insert(const T& item);
+        // return a pair of iterators such that all occurrences
+        // of item are in the iterator range 
+        // [first member of pair, second member of pair)
+        std::pair<iterator, iterator> equal_range(const T& item);
+
+        //constant version
+        std::pair<const_iterator, const_iterator> equal_range(const T& item) const;
+
+        // insert item into the multiset and return an
+        // iterator pointing at the new element.
+        // PostCondition: the element item is added to the multiset
+        iterator insert(const T& item);
         
-        // if item is in the set, erase it and return 1;
-        // otherwise, return 0
-        // Postcondition: the set size decreases by 1 if item is
-        // in the set
+        // erase all occurrences of item from the multiset
+        // and return the number of items erased.
+        // Postcondition: the multiset size decreases by the number of occurrences 
+        // of item in the multiset
         int erase(const T& item);
 
         // erase the item pointed to by pos.
-        // Preconditions: the set is not empty and pos points
-        // to an item in the set. if the set is empty, the
+        // Preconditions: the multiset is not empty and pos points
+        // to an item in the multiset. if the multiset is empty, the
         // function throws the UnderflowError exception. if the
         // iterator is invalid, the function throws the ReferenceError
         // exception.
-        // Postcondition: the set size decreases by 1
+        // Postcondition: the multiset size decreases by 1
         void erase(iterator pos);
 
         // erase the elements in the range [first, last)
-        // Precondition: the set is not empty. if the set is empty,
+        // Precondition: the multiset is not empty. if the multiset is empty,
         // the function throws the UnderflowError exception.
-        // Postcondition: the set size decreases by the number
+        // Postcondition: the multiset size decreases by the number
         // elements in the range
         void erase(iterator first, iterator last);
 
         // return an iterator pointing at the first member
-        // in the set
+        // in the multiset
         iterator begin();
 
         // constant version of begin()
         const_iterator begin() const;
 
         // return an iterator pointing just past the last
-        // member in the set
+        // member in the multiset
         iterator end();
 
         // constant version of end()
         const_iterator end() const;
 
     private:
-        // set implemented using a binary search tree
-        BSTree<T> t;
+        // multiset implemented using a multi binary search tree
+        MBSTree<T> t;
 };
 
 // CONSTRUCTORS
 
 template <typename T>
-Set<T>::Set()
+MSet<T>::MSet()
 {}
 
 template <typename T>
-Set<T>::Set(T *first, T *last): t(first, last)
+MSet<T>::MSet(T *first, T *last): t(first, last)
 {}
 
 template <typename T>
-bool Set<T>::empty() const
+bool MSet<T>::empty() const
 {
     return t.empty();
 }
 
 template <typename T>
-int Set<T>::size() const
+int MSet<T>::size() const
 {
     return t.size();
 }
 
 template <typename T>
-Set<T>::iterator Set<T>::find (const T& item)
+int MSet<T>::count(const T& item) const
 {
-    // return BSTree iterator which is a Set iterator
+    return t.count(item);   
+}
+
+std::pair<MSet<T>::iterator,
+          MSet<T>::iterator> 
+MSet<T>::equal_range(const T& item)
+{
+    return t.equal_range(item);
+}
+
+std::pair<MSet<T>::const_iterator,
+          MSet<T>::const_iterator> 
+MSet<T>::equal_range(const T& item) const
+{
+    return t.equal_range(item);
+}
+
+template <typename T>
+MSet<T>::iterator MSet<T>::find (const T& item)
+{
+    // return MBSTree iterator which is a MSet iterator
     return t.find(item);
 }
 
 template <typename T>
-Set<T>::const_iterator Set<T>::find (const T& item) const
+MSet<T>::const_iterator MSet<T>::find (const T& item) const
 {
-    // return BSTree iterator which is a Set iterator
+    // return MBSTree iterator which is a MSet iterator
     return t.find(item);
 }
 
 template <typename T>
-std::pair<Set<T>::iterator,bool> Set<T>::insert(const T& item)
+MSet<T>::iterator MSet<T>::insert(const T& item)
 {
-    // insert item into the binary search tree and return
-    // the iterator-bool pair
     return t.insert(item);
 }
 
 template <typename T>
-int Set<T>::erase(const T& item)
+int MSet<T>::erase(const T& item)
 {
-    // erase item from the tree
     return t.erase(item);
 }
 
 template <typename T>
-void Set<T>::erase(iterator pos)
+void MSet<T>::erase(iterator pos)
 {
     if (t.size() == 0)
-        throw UnderflowError("Set erase(): set is empty");
+        throw UnderflowError("MSet erase(): set is empty");
 
     if (pos == end())
-        throw ReferenceError("Set erase(): invalid iterator");
+        throw ReferenceError("MSet erase(): invalid iterator");
 
-    // erase the item in the tree pointed to by pos
     t.erase(pos);
 }
 
 template <typename T>
-void Set<T>::erase(iterator first, iterator last)
+void MSet<T>::erase(iterator first, iterator last)
 {
     if (t.size() == 0)
-        throw UnderflowError("Set erase(): set is empty");
+        throw UnderflowError("MSet erase(): set is empty");
 
-    // erase range [first, last) in the tree
     t.erase(first, last);
 }
 
 template <typename T>
-Set<T>::iterator Set<T>::begin()
+MSet<T>::iterator MSet<T>::begin()
 {
-    // a Set iterator is an BSTree iterator
+    // a MSet iterator is an MBSTree iterator
     return t.begin();
 }
 
 template <typename T>
-Set<T>::const_iterator Set<T>::begin() const
+MSet<T>::const_iterator MSet<T>::begin() const
 {
     return t.begin();
 }
 
 template <typename T>
-Set<T>::iterator Set<T>::end()
+MSet<T>::iterator MSet<T>::end()
 {
     return t.end();
 }
 
 template <typename T>
-Set<T>::const_iterator Set<T>::end() const
+MSet<T>::const_iterator MSet<T>::end() const
 {
     return t.end();
 }
@@ -188,30 +209,30 @@ Set<T>::const_iterator Set<T>::end() const
 // determine if sets lhs and rhs have the same size and
 // are equal element by element
 template <typename T>
-bool operator== (const Set<T>& lhs, const Set<T>& rhs);
+bool operator== (const MSet<T>& lhs, const MSet<T>& rhs);
 
-// return a Set object containing all elements that
+// return a MSet object containing all elements that
 // are in lhs or rhs
 template <typename T>
-Set<T> operator+ (const Set<T>& lhs, const Set<T>& rhs);
+MSet<T> operator+ (const MSet<T>& lhs, const MSet<T>& rhs);
 
-// return a Set object containing all elements that
+// return a MSet object containing all elements that
 // are in goth lhs and rhs
 template <typename T>
-Set<T> operator* (const Set<T>& lhs, const Set<T>& rhs);
+MSet<T> operator* (const MSet<T>& lhs, const MSet<T>& rhs);
 
-// return a Set object containing all elements that
+// return a MSet object containing all elements that
 // are in lhs but not in rhs
 template <typename T>
-Set<T> operator- (const Set<T>& lhs, const Set<T>& rhs);
+MSet<T> operator- (const MSet<T>& lhs, const MSet<T>& rhs);
 
 
 // SET FUNCTION IMPLEMENTATIONS
 
 template <typename T>
-bool operator== (const Set<T>& lhs, const Set<T>& rhs)
+bool operator== (const MSet<T>& lhs, const MSet<T>& rhs)
 {
-    Set<T>::const_iterator myself = lhs.begin(), other = rhs.begin();
+    MSet<T>::const_iterator myself = lhs.begin(), other = rhs.begin();
 
     // return false if the sets do not have the same size
     if (lhs.size() == rhs.size())
@@ -232,12 +253,12 @@ bool operator== (const Set<T>& lhs, const Set<T>& rhs)
 }
 
 template <typename T>
-Set<T> operator+ (const Set<T>& lhs, const Set<T>& rhs)
+MSet<T> operator+ (const MSet<T>& lhs, const MSet<T>& rhs)
 {
     // constuct union
-    Set<T> setUnion;
+    MSet<T> setUnion;
     // iterators that traverse the sets
-    Set<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
+    MSet<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
 
     // move forward as long as we have not reached the end of
     // either set
@@ -289,12 +310,12 @@ Set<T> operator+ (const Set<T>& lhs, const Set<T>& rhs)
 }
 
 template <typename T>
-Set<T> operator* (const Set<T>& lhs, const Set<T>& rhs)
+MSet<T> operator* (const MSet<T>& lhs, const MSet<T>& rhs)
 {
     // constuct intersection
-    Set<T> setIntersection;
+    MSet<T> setIntersection;
     // iterators that traverse the sets
-    Set<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
+    MSet<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
 
     // move forward as long as we have not reached the end of
     // either set
@@ -322,12 +343,12 @@ Set<T> operator* (const Set<T>& lhs, const Set<T>& rhs)
 }
 
 template <typename T>
-Set<T> operator- (const Set<T>& lhs, const Set<T>& rhs)
+MSet<T> operator- (const MSet<T>& lhs, const MSet<T>& rhs)
 {
     // construct difference
-    Set<T> setDifference;
+    MSet<T> setDifference;
     // iterators that traverse the sets
-    Set<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
+    MSet<T>::const_iterator lhsIter = lhs.begin(), rhsIter = rhs.begin();
 
     // move forward as long as we have not reached the end of
     // either set
@@ -368,4 +389,4 @@ Set<T> operator- (const Set<T>& lhs, const Set<T>& rhs)
     return setDifference;
 }
 
-#endif  // _SET_H_
+#endif  // _MSET_H_
